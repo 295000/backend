bf16430a1f8c39e517ef2f7ff3ddc8db86d232a4
aluminum platinum <kashmeerkerla@gmail.com>
2022-08-28 16:32:19 +0530

    assignment complete

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -14,7 +14,42 @@ const userSchema = new mongoose.Schema( {
         type: String,
         enum: ["male", "female", "other"]
     },
+    isDeleted: {
+        type:Boolean,
+        default:false
+    },
     age: Number,
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema)
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema( {
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+
+//         required: true
+//     },
+//     emailId: String,
+//     password: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "other"]
+//     },
+//     age: Number,
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema)
