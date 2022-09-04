31d4715ecc9c1271267bb58a8d36b2a91c81dddd
aluminum platinum <kashmeerkerla@gmail.com>
2022-09-04 22:01:10 +0530

    assignment complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const CowinController= require("../controllers/cowinController")
-
+const weatherController=require("../Controllers/weatherController")
+const memesController=require("../controllers/memesController")
 
 
 router.get("/test-me", function (req, res) {
@@ -12,8 +13,17 @@ router.get("/test-me", function (req, res) {
 router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
-
 router.post("/cowin/getOtp", CowinController.getOtp)
+//////////////////  1-09-assignment/////////////
+router.get("/cowin/districtId",CowinController.getSessionsByDistrict)
+router.get("/weather/london",weatherController.weather)
+router.get("/tempOfLondon",weatherController.tempOfLondon);
+router.get("/tempOfCities",weatherController.tempOfCities);
+router.get("/getMemes", memesController.getMeme)
+router.post("/createMemes", memesController.createMeme)
+
+
+
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
